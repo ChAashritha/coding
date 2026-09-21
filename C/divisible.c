213f4ee0c76affe918630fd0d542115364666ee9
CHUNDRU AASHRITHA <55d8aashritha@gmail.com>
2026-09-21 18:56:46 +0530

    daily practice

---
+++ C/divisible.c
@@ -0,0 +1,12 @@
+/*Five step oath of Pandavas*/
+#include<stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%5==0 && n>0)
+    printf("Royal Approved");
+    else
+    printf("Rejected");
+
+}
---
+++ C/test .c
@@ -0,0 +1,11 @@
+/*Test of young warriors*/
+#include<stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>=40)
+    printf("Ready for Battle");
+    else
+    printf("Continue Training");
+}
